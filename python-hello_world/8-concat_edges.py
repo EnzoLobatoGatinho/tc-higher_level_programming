08f5c73b806eb8c465b6c3b6f249b3d43cc9dd4b
EnzoLobatoGatinho <Enzolobatoga@gmail.com>
2025-07-16 13:24:29 +0000

    Adiciona 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted"
-str1 = "interactive"
-str2 = "object-oriented programming"
-print(f"{str}, {str1}, {str2}/")
+str = "Python is an interpreted, interactive, object-oriented programming " \
+      "language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:113] + str[:6]
+print(str)
