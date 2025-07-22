183803c96aed19eda6240a3be800d23c45193c20
EnzoLobatoGatinho <Enzolobatoga@gmail.com>
2025-07-22 23:47:50 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming " \
-      "language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[106:112] + str[:6]
-print(str)
+import this
+print(this.s[39:66] +
+      this.s[133:154] +
+      this.s[5:6] +
+      this.s[97:98] +
+      this.s[6:7] +
+      this.s[154:161])
