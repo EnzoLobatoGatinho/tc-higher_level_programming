67d7eb2bf1e35a8fba43b016f8b84d53a9c3f122
EnzoLobatoGatinho <Enzolobatoga@gmail.com>
2025-07-25 19:18:12 +0000

    add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import this
-print(this.s[39:66] + this.s[133:154] + this.s[5:6] + this.s[97:98] + this.s[6:7] + this.s[154:161])
-# linha 4 - comentário
-# linha 5 - comentário
-
+print(this.s[39:66] +
+      this.s[133:154] +
+      this.s[5:6] + this.s[97:98] + this.s[6:7] + this.s[154:161])
