81a468537f68e8b568a240413880c5c0fdb93a4c
rpontodev <Enzolobatoga@gmail.com>
2025-09-17 20:18:14 -0300

    modified:   python-if_else_loops_functions/5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
+
 for number in range(00, 99):
         print("{:02d}".format(number), end=", ")
         if number == 98:
-            print(99)
-            break
+            print("99")
