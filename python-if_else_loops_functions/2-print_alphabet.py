d9070c87575c345941be1b28a7dd4526afb8a736
EnzoLobatoGatinho <Enzolobatoga@gmail.com>
2025-09-18 21:31:04 +0000

    modified:   2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("{}".format("".join([chr(number) for number in range(ord("a"), ord("z")+1)])), end="")
+print("{}".format("".join([chr(number) for number in range(ord("a"), ord("z")+1)])),
+      end="")
