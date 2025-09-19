0a2f7fbe18b55a493e3f01fdd6c79d54f7f04b5b
rpontodev <Enzolobatoga@gmail.com>
2025-09-19 19:41:19 -0300

    new file:   python-if_else_loops_functions/3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -4,7 +4,3 @@ print("{}".format("".join(
     [chr(number) for number in range(ord("a"), ord("z") + 1)
      if chr(number) not in ("e", "q")]
 )), end="")
-<<<<<<< HEAD
-=======
-
->>>>>>> 85ec1c58c22cf75da1bd9b807c47a3aa459f4901
